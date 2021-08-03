91fad2c95ca3452f73fbe48c67e0dfba0d2797fb
oliveiramiro2 <ramiromares95@gmail.com>
2021-08-02 23:40:33 -0300

    começando a fazer a home pag

--- pokemons/src/components/PokedexHome.js
+++ pokemons/src/components/PokedexHome.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function PokedexHome(){
 
     return(
-        <div>
-            <h1>Home</h1>
+        <div className='container-home'>
+            <div className='container-home-img'>
+                <h3>Pokedex</h3>
+                <hr /><hr /><hr />
+                <div className='rodape-home-links'>
+                    <Link className='texto-padrao' to='/pokemons'>Todos os pokémons</Link>
+                </div>
+            </div>
         </div>
     )
 }
