fe63fd1e512769b0615342466298683b7ad793ac
oliveiramiro2 <ramiromares95@gmail.com>
2021-08-08 01:01:59 -0300

    mostrando sprites

--- pokemons/src/components/InfoPoke.js
+++ pokemons/src/components/InfoPoke.js
@@ -4,19 +4,34 @@ import changeDados from './../store/action/informacaoA'
 
 function InfoPoke({ poke, info, changeDados }){
     React.useEffect(()=>{
-        changeDados(poke.poke)
+        if(poke.poke){
+            changeDados(poke.poke)
+            localStorage.setItem("numPoke", poke.poke)
+        }else{
+            changeDados(localStorage.getItem("numPoke"))
+        }
     }, [changeDados, poke])
     if(info){
-        console.log(info.info.sprites)
         return(
             <div className='pokedex-container'>  
                 <p>Informações 1</p>
-                <div className='pokedex-fundo'>                                      
-                    <img width='200' height='250' src={info.info.sprites.front_default}></img>
-                    <img width='200' height='250' src={info.info.sprites.back_default}></img>
-                    <img width='200' height='250' src={info.info.sprites.front_shiny}></img>
-                    <img width='200' height='250' src={info.info.sprites.back_shiny}></img>
-                    <img width='200' height='250' src={info.info.sprites.other.dream_world.front_default}></img>
+                <div className='pokedex-fundo'>                   
+                    <div className='container-fotos'>
+                        <div>
+                            <p className='texto-padrao'>Sprites Padrão</p>
+                            <img width='100' height='150' alt='frente' src={info.info.sprites.front_default}></img>
+                            <img width='100' height='150' alt='costas' src={info.info.sprites.back_default}></img>
+                        </div>
+                        <div>
+                            <p className='texto-padrao'>Sprites Shiny</p>
+                            <img width='100' height='150' alt='shiny-frente' src={info.info.sprites.front_shiny}></img>
+                            <img width='100' height='150' alt='shiny-costas' src={info.info.sprites.back_shiny}></img>
+                        </div>
+                        <div>
+                            <p className='texto-padrao'>Sprites Dream World</p>
+                            <img width='100' height='150' alt='dream-frente' src={info.info.sprites.other.dream_world.front_default}></img>
+                        </div>
+                    </div>                                      
                 </div>        
                 <p>Informações 2</p>
             </div>
